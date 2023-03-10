777ba33b59b99ef4d3cc585851def3541ffa4ac3
Ersan <ersanaksahin@gmail.com>
2023-03-10 10:10:21 +0300

    Add files via upload

---
+++ AnimexEntertainmentNetwork.IOC/Bootstrapper.cs
@@ -0,0 +1,50 @@
+using System.Web.Mvc;
+using Microsoft.Practices.Unity;
+using Unity.Mvc4;
+
+namespace AnimexEntertainmentNetwork.IOC
+{
+    public static class IOCExtensions
+    {
+        public static void BindInRequestScope<T1, T2>(this IUnityContainer container) where T2 : T1
+        {
+            container.RegisterType<T1, T2>(new HierarchicalLifetimeManager());
+        }
+
+        public static void BindInSingletonScope<T1, T2>(this IUnityContainer container) where T2 : T1
+        {
+            container.RegisterType<T1, T2>(new ContainerControlledLifetimeManager());
+        }
+    }
+
+    public static class Bootstrapper
+    {
+        public static IUnityContainer Initialise()
+        {
+            var container = BuildUnityContainer();
+
+            DependencyResolver.SetResolver(new UnityDependencyResolver(container));
+
+            return container;
+        }
+
+        private static IUnityContainer BuildUnityContainer()
+        {
+            var container = new UnityContainer();
+
+            // register all your components with the container here
+            // it is NOT necessary to register your controllers
+
+            // e.g. container.RegisterType<ITestService, TestService>();    
+            RegisterTypes(container);
+
+            return container;
+        }
+
+        public static void RegisterTypes(IUnityContainer container)
+        {
+
+
+        }
+    }
+}
