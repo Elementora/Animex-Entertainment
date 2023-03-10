317a6bc3455d54c8bd835076c093d4615248b24b
Ersan <ersanaksahin@gmail.com>
2023-03-10 10:07:31 +0300

    Add files via upload

---
+++ AnimexEntertainmentNetwork.Aut/AntiXSS.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web.Util;
+
+namespace AnimexEntertainmentNetwork.Aut
+{
+    public class AntiXSS : HttpEncoder
+    {
+        public AntiXSS()
+        {
+
+        }
+
+        protected override void HtmlEncode(string value, TextWriter output)
+        {
+            base.HtmlEncode(value, output);
+        }
+        protected override void HtmlAttributeEncode(string value, TextWriter output)
+        {
+            base.HtmlAttributeEncode(value, output);
+        }
+        protected override void HeaderNameValueEncode(string headerName, string headerValue, out string encodedHeaderName, out string encodedHeaderValue)
+        {
+            base.HeaderNameValueEncode(headerName, headerValue, out encodedHeaderName, out encodedHeaderValue);
+        }
+        protected override void HtmlDecode(string value, System.IO.TextWriter output)
+        {
+            base.HtmlDecode(value, output);
+        }
+
+        protected override byte[] UrlEncode(byte[] bytes, int offset, int count)
+        {
+            return base.UrlEncode(bytes, offset, count);
+        }
+
+        protected override string JavaScriptStringEncode(string value)
+        {
+            return base.JavaScriptStringEncode(value);
+        }
+
+        protected override string UrlPathEncode(string value)
+        {
+            return base.UrlPathEncode(value);
+        }
+
+
+
+    }
+}
---
+++ AnimexEntertainmentNetwork.Aut/AutControl.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Security;
+
+namespace AnimexEntertainmentNetwork.Aut
+{
+    public class AutControl : AuthorizeAttribute
+    {
+        protected override bool AuthorizeCore(HttpContextBase httpContext)
+        {
+
+            if (httpContext.Request.Cookies[FormsAuthentication.FormsCookieName] == null)
+
+                return false;
+            else
+                return true;
+
+        }
+        protected override void HandleUnauthorizedRequest(AuthorizationContext filterContext)
+        {
+
+            filterContext.Result = new RedirectResult("/Home?ReturnUrl=" + filterContext.HttpContext.Request.Url.PathAndQuery);
+        }
+
+    }
+}
